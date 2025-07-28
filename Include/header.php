49bf2ab5935c27dfd59bf3967d27563ddad295c3
Rajan-908 <studentrajan908@email.com>
2025-07-28 21:25:41 +0545

    navigation file creaded

---
+++ Include/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header</title>
+</head>
+<body>
+<nav>
+    <div class="container">
+        <div class="logo"></div>
+         <ul class="navlink">
+         <li><a href="Index.html"></a></li>  
+         <li><a href="About"></a></li>
+         <li><a href="Contact"></a></li>
+         <button><a href="Login.php"></a></button>
+         <button><a href="Register as Donor"></a></button>
+         </ul>
+        </div>
+    </nav>
+</body>
+</html>
