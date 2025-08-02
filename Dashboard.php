ac289d516b60e29fb1e0935c5bec57f2e568592e
Rajan-908 <studentrajan908@email.com>
2025-08-02 18:25:24 +0545

    user dashboard content added

--- Dashboard.php
+++ Dashboard.php
@@ -6,7 +6,43 @@
     <title>Document</title>
 </head>
 <body>
-    <?php include '../V-project/Include/header.php' ?>
     
+<div class="profile">
+    <div class="profile-head">
+        <h1 id="name">Rajan Sharma</h1>
+    </div>
+    <div class="profile-body">
+        <ul>
+            <li>id</li>
+            <li>blood group</li>
+            <li>lstat</li>
+        </ul>
+        <button>Edit profile</button>
+    </div>
+</div>
+<div class="donation-container">
+<div class="do-head">Your donation Impact</div>
+<div class="do-co-body">
+    <div>
+        <h4>Total Donations</h4>
+        <h2 id="count"></h2>
+    </div>
+    <div>
+        <h4>Lives Impacted</h4>
+        <h2 id="count1"></h2>
+    </div>
+    <div>
+        <h4>Next Milestone</h4>
+        <h2 id="count2"></h2>
+    </div>
+    <div class="graph"></div>
+</div>
+</div>
+<div class="do-history">
+    <div class="do-head">Recent Donations</div>
+    <div class="do-body">
+        ........
+    </div>
+</div>
 </body>
 </html>
