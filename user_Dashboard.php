8cfd9b0622d41fca0cefc1f9776b8869aa1f37f8
Rajan-908 <studentrajan908@email.com>
2025-08-03 11:25:29 +0545

    user profile rename and dashboard created

---
+++ user_Dashboard.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="Css/dashboard.css">
+</head>
+<body>
+    <nav>
+        <div class="header">
+            <div class="logo"><img src="../V-project/Images/logo.jpg" alt="missisng image"></div>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="#about">About Us</a></li>
+                <li><a href="logout.php">Log out</a></li>
+            </ul>
+        </div>
+    </nav>
+<div class="profile">
+    <div class="profile-head">
+        <h1 id="name">Rajan Sharma</h1>
+    </div>
+    <div class="profile-body">
+        <ul>
+            <li>id</li>
+            <li>blood group</li>
+            <li>lstat</li>
+        </ul>
+        <button>Edit profile</button>
+    </div>
+</div>
+<div class="donation-container">
+<div class="do-head">Your donation Impact</div>
+<div class="do-co-body">
+    <div>
+        <h4>Total Donations</h4>
+        <h2 id="count"></h2>
+    </div>
+    <div>
+        <h4>Lives Impacted</h4>
+        <h2 id="count1"></h2>
+    </div>
+    <div>
+        <h4>Next Milestone</h4>
+        <h2 id="count2"></h2>
+    </div>
+    <div class="graph"></div>
+</div>
+</div>
+<div class="do-history">
+    <div class="do-head">Recent Donations</div>
+    <div class="do-body">
+        ........
+    </div>
+</div>
+</body>
+</html>
